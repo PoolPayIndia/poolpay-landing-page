6f307d039ac953b207f64187eaaa644071008d68
Sarvesh-Kedia <sarvesh512kedia@gmail.com>
2022-09-08 12:12:04 +0530

    updated faqs

--- js/faqs.js
+++ js/faqs.js
@@ -6,65 +6,48 @@ var count = 0
 fetch('../assets/files/faqs.json')
 	.then(response => response.json())
 	.then(jsonResponse => {
-		faqs = jsonResponse
 
-		console.log(faqs)
+		faqlist = jsonResponse
+		currhead = null
 		
-		faqs.forEach(function(parentObj){
+		faqlist.forEach(function(faq){
 
+			if(currhead != faq.topic) {
+				currhead = faq.topic
+
+				heading = `<h3 class="heading">`+faq.topic+`</h3>`
+				nodeEntry.insertAdjacentHTML('beforeend','<br />');
+				nodeEntry.insertAdjacentHTML('beforeend', heading);
+			}
+
+
+			newItem = `
+				<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
+				<h2 itemprop="name" class="accordion-header" id="heading` + count + `">
+				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` + count + `" aria-expanded="false" aria-controls="collapse` + count + `">
+					` + faq.question + `
+				<div style="float:right;">
+					<i class="bi bi-chevron-up"></i>
+				</div>
+				<div style="float:right;">
+					<i class="bi bi-chevron-down"></i>
+				</div>
+				</button>
+				</h2>
+				<div id="collapse` + count + `" class="accordion-collapse collapse" aria-labelledby="heading` + count + `" data-bs-parent="#accordionExample" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+				<div class="accordion-body" itemprop="text">
+					` + faq.answer + `
+				</div>
+				</div>
+				</div>
 			
-			// console.log(parentObj.children)
+			`
 			
-			heading = `<h3 class="heading">`+parentObj.topic+`</h3>`
-			nodeEntry.insertAdjacentHTML('beforeend','<br />');
-			nodeEntry.insertAdjacentHTML('beforeend', heading);
-
-			parentObj.children.forEach(function(obj){
-
-				newItem =
-
-					`
-					<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
-					<h2 itemprop="name" class="accordion-header" id="heading` + count + `">
-					<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` + count + `" aria-expanded="false" aria-controls="collapse` + count + `">
-						` + obj.question + `
-					<div style="float:right;">
-						<i class="bi bi-chevron-up"></i>
-					</div>
-					<div style="float:right;">
-						<i class="bi bi-chevron-down"></i>
-					</div>
-					</button>
-					</h2>
-					<div id="collapse` + count + `" class="accordion-collapse collapse" aria-labelledby="heading` + count + `" data-bs-parent="#accordionExample" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-					<div class="accordion-body" itemprop="text">
-						` + obj.answer + `
-					</div>
-					</div>
-					</div>
-				
-					`
-				
-				
-				nodeEntry.insertAdjacentHTML('beforeend',newItem);
-			
-				count++;
-			})
+			nodeEntry.insertAdjacentHTML('beforeend',newItem);
+		
+			count++;
+	
 		
 		})
 
 	}) 
-
-
-					
-/* 
-https://jsonifyit.com/
-
-step 2.
-header - yes
-
-step 3.
-base structure - records
-name key - add level - topic
-update preview
-*/
