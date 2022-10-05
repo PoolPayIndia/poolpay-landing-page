84e3347fa49319725374635066f198fb9c366aa5
Sarvesh-Kedia <sarvesh512kedia@gmail.com>
2022-10-05 12:43:49 +0530

    faq page error fixed

--- js/faqs.js
+++ js/faqs.js
@@ -23,24 +23,23 @@ fetch('../assets/files/faqs.json')
 
 			newItem = `
 				<div itemscope itemprop="mainEntity" itemtype="https://schema.org/Question" class="accordion-item">
-				<h2 itemprop="name" class="accordion-header" id="heading` + count + `">
-				<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` + count + `" aria-expanded="false" aria-controls="collapse` + count + `">
-					` + faq.question + `
-				<div style="float:right;">
-					<i class="bi bi-chevron-up"></i>
-				</div>
-				<div style="float:right;">
-					<i class="bi bi-chevron-down"></i>
-				</div>
-				</button>
-				</h2>
-				<div id="collapse` + count + `" class="accordion-collapse collapse" aria-labelledby="heading` + count + `" data-bs-parent="#accordionExample" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
-				<div class="accordion-body" itemprop="text">
-					` + faq.answer + `
-				</div>
-				</div>
-				</div>
-			
+					<h2 itemprop="name" class="accordion-header" id="heading` + count + `">
+						<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse` + count + `" aria-expanded="false" aria-controls="collapse` + count + `">
+							` + faq.question + `
+						<div style="float:right;">
+							<i class="bi bi-chevron-up"></i>
+						</div>
+						<div style="float:right;">
+							<i class="bi bi-chevron-down"></i>
+						</div>
+						</button>
+					</h2>
+					<div id="collapse` + count + `" class="accordion-collapse collapse" aria-labelledby="heading` + count + `" data-bs-parent="#accordionExample" itemscope itemprop="acceptedAnswer" itemtype="https://schema.org/Answer">
+						<div class="accordion-body" itemprop="text">
+							` + faq.answer + `
+						</div>
+					</div>
+				</div>	
 			`
 			
 			nodeEntry.insertAdjacentHTML('beforeend',newItem);
